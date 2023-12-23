9b526046e2ec8f726f69cee9988d7d1cc2dd1315
Gensha5 <137396801+gensha5@users.noreply.github.com>
2023-12-23 19:51:54 +0900

    feat: app.pyからresponseを渡す

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 import streamlit as st
 import requests
+
+st.title("自由記述欄を自動分析する")
+
+responses = st.text_area("自由記述欄の回答を入力してください。回答はコンマで区切ってください", height=300)
+
+if st.button("分析する"):
+    response = requests.post("http://localhost:8000/analyze", data={"responses": responses})
+    results = response.json()
+
+    for sentiment, items in results.items():
+        st.subheader(f"{sentiment.title()} Responses")
+        st.write(f"回答数: {len(items)}")
+        st.write("回答:")
+        for item in items:
+            st.text(item)
