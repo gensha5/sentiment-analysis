1ad2b9b7467c31cf837042f16ab282ed620513c4
Gensha5 <137396801+gensha5@users.noreply.github.com>
2023-12-23 19:42:54 +0900

    feat: gptから回答を得る

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from fastapi import FastAPI, Form
-import openai
+from openai import OpenAI
 import os
+
+app = FastAPI()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
+
+@app.get("/analyze")
+async def analyze(responses: str = Form(...)):
+    respose_list = responses.split(",")
+
+    positive_responses = []
+    negative_responses = []
+    neutral_responses = []
+
+    for response in respose_list:
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messeages=[
+                {"role": "user", "content": f"Classify the sentiment of the following text as positive, negative, or neutral:\n\n{response}"}
+            ]
+        )
