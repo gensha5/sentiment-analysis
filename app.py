b4a13b5151bace883e0ffd33436f1e34e6faa751
Gensha5 <137396801+gensha5@users.noreply.github.com>
2023-12-23 23:10:54 +0900

    change: app.pyをわかりやすく修正

--- app.py
+++ app.py
@@ -3,15 +3,30 @@ import requests
 
 st.title("自由記述欄を自動分析する")
 
+question = st.text_input("質問文を入力してください。")
 responses = st.text_area("自由記述欄の回答を入力してください。\n1回答1行で入力してください。", height=300)
 
 if st.button("分析する"):
-    response = requests.post("https://sentiment-analysis-lf56.onrender.com", data={"responses": responses})
+    response = requests.post("https://sentiment-analysis-lf56.onrender.com", data={"question":question, "responses": responses})
     results = response.json()
 
-    for sentiment, items in results.items():
-        st.subheader(f"{sentiment.title()}")
-        st.write(f"回答数: {len(items)}")
+    if "positive" in results:
+        st.subheader("ポジティブ")
+        st.write(f"回答数: {len(results['positive'])}")
         st.write("回答:")
-        for item in items:
+        for item in results["positive"]:
+            st.text(item)
+
+    if "negative" in results:
+        st.subheader("ネガティブ")
+        st.write(f"回答数: {len(results['negative'])}")
+        st.write("回答:")
+        for item in results["negative"]:
+            st.text(item)
+
+    if "neutral" in results:
+        st.subheader("ニュートラル")
+        st.write(f"回答数: {len(results['neutral'])}")
+        st.write("回答:")
+        for item in results["neutral"]:
             st.text(item)
