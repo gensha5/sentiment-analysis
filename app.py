d100ac25b00b20f70a29cad9ac77cd21e9f0fe65
Gensha5 <137396801+gensha5@users.noreply.github.com>
2023-12-23 21:28:01 +0900

    change:

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("自由記述欄を自動分析する")
 responses = st.text_area("自由記述欄の回答を入力してください。回答はコンマで区切ってください", height=300)
 
 if st.button("分析する"):
-    response = requests.post("http://localhost:8000/analyze", data={"responses": responses})
+    response = requests.post("https://sentiment-analysis-lf56.onrender.com", data={"responses": responses})
     results = response.json()
 
     for sentiment, items in results.items():
